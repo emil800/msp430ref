33ebe52398aac6c13e69f836241c3a65d66452a2
emil800 <emil.stoyanov@eonas.de>
2024-07-18 15:32:29 +0200

    elf file should be flashed

--- flash.sh
+++ flash.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 # Flash using mspdebug
 echo -e  "Programming using mspdebugger"
 
-mspdebug rf2500 "prog $SCRIPT_DIR/bin/main.out"
+mspdebug rf2500 "prog $SCRIPT_DIR/bin/main.elf"
 
